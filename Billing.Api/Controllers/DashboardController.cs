4428eb54aba28fb15ede9dee25327ae2f5a5de7c
anurbecirovic <annurbecirovic@gmail.com>
2017-03-16 13:29:36 +0100

    Dashboard

---
+++ Billing.Api/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Billing.Api.Models;
+using Billing.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Billing.Api.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        public IHttpActionResult Get() {
+
+            DashboardModel result = new DashboardModel(8);
+            return Ok(result);
+        }
+
+
+    }
+}
