0a9428d98570bf55d8946ba97ffe7b40ae404e63
anurbecirovic <annurbecirovic@gmail.com>
2017-03-22 09:16:15 +0100

    category report done

---
+++ Billing.Api/Controllers/SalesByCategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Billing.Api.Helpers;
+using Billing.Api.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Billing.Api.Controllers
+{
+    public class SalesByCategoryController : BaseController
+    {
+
+        //[BillingAuthorization] DODATI NAKNADNO
+        public IHttpActionResult Post(RequestModel request)
+        {
+            {
+                try
+                {
+                    return Ok(SalesByCategoryReport.Report(UnitOfWork, request));
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Billing.Api/Models/SalesByCategoryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Billing.Api.Models
+{
+    public class CategoriesSalesModel {
+        public string CategoryName { get; set; }
+        public double CategoryTotal { get; set; }
+        public double CategoryPercent { get; set; }
+    }
+    public class SalesByCategoryModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double GrandTotal { get; set; }
+
+        public List<CategoriesSalesModel> Categories { get; set; }
+    }
+}
