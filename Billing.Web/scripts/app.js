b22e513cca45f3cc2ecdadd4feb83fa8574b2876
easee <salchind@gmail.com>
2017-03-31 09:36:00 +0200

    Commented foreach in TokenAuthorizationAttribute

--- Billing.Web/scripts/app.js
+++ Billing.Web/scripts/app.js
@@ -1,5 +1,6 @@
+(function(){
 
-
+    authenticated = false;
     var app = angular.module("Billing", ["ngRoute"]);
 
     app.config(function($routeProvider){
@@ -10,7 +11,17 @@
             .when("/customers", {
                 templateUrl: "views/customers.html",
                 controller: "CustomersCtrl" })
+            .when("/login", {
+                templateUrl: "views/login.html",
+                controller: "LoginCtrl" })
             .otherwise({ redirectTo: "/agents" });
+    }).run(function($rootScope, $location){
+        $rootScope.$on("$routeChangeStart", function(event, next, current){
+            if(!authenticated){
+                if(next.templateUrl != "views/login.html"){
+                    $location.path("/login");
+                }
+            }
+        })
     });
-
-
+}());
