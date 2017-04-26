f8a19c2047f7ca00064d1fc1bc64fed157009fd3
JosipKvesic <jkvesic1@etf.unsa.ba>
2017-04-26 14:26:23 +0200

    Sales by region is fixed

--- Billing.Web/scripts/controllers/ReportCtrl/StockLevelCtrl.js
+++ Billing.Web/scripts/controllers/ReportCtrl/StockLevelCtrl.js
@@ -1,19 +1,10 @@
 (function(){
-    application.controller("StockLevelCtrl",['$scope','$rootScope','$anchorScroll','DataService',function($scope,$rootScope,$anchorScroll,DataService){
-       
-        
-        function ListAll(){
-            DataService.insert("productsbycategory",$scope.request,function(data){
-               $scope.Inventory=data; 
-            });
-        };
-        
-        function ListCategories(){
-            DataService.list("categories",function(data){
-              $scope.categories=data;
-          });  
-        };
-        
+    app.controller("StockLevelCtrl", ['$scope', '$anchorScroll', 'DataService', function($scope, $anchorScroll, DataService) {
+     ListCategories();
+     function ListCategories(){
+            DataService.list("categories", function(data){ $scope.categories = data});
+            
+        };      
         
     }]);
 }());
