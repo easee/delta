b27b5126ac19c5498d4643b3ddca3afd6fc5f123
JosipKvesic <jkvesic1@etf.unsa.ba>
2017-04-27 16:33:42 +0200

    Print is added

--- Billing.Web/scripts/controllers/ReportCtrl/InvoicesReviewCtrl.js
+++ Billing.Web/scripts/controllers/ReportCtrl/InvoicesReviewCtrl.js
@@ -33,6 +33,13 @@
                 return _selected;
             }
         };
+              $scope.printDiv = function (divName) {
+           var printContents = document.getElementById(divName).innerHTML;
+           var popupWin = window.open('', '_blank', 'width=1000,height=1000');
+           popupWin.document.open();
+           popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" /><link rel="stylesheet" type="text/css" href="styles/style.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
+           popupWin.document.close();
+       };
 
         $scope.modelOptions = {
             debounce: {
