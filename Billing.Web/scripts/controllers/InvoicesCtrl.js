e81a92fd02b0f831f1da14941e58ded0f01a223a
anurbecirovic <annurbecirovic@gmail.com>
2017-04-28 10:00:49 +0200

    datepick ugly but working

--- Billing.Web/scripts/controllers/InvoicesCtrl.js
+++ Billing.Web/scripts/controllers/InvoicesCtrl.js
@@ -23,18 +23,18 @@
         };
         //--------------------- 
 
-        $scope.printDiv = function (divName) {
-           var printContents = document.getElementById(divName).innerHTML;
-           var popupWin = window.open('', '_blank', 'width=1000,height=1000');
-           popupWin.document.open();
-           popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" /><link rel="stylesheet" type="text/css" href="styles/style.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
-           popupWin.document.close();
-       };
-        
+        $scope.printDiv = function(divName) {
+            var printContents = document.getElementById(divName).innerHTML;
+            var popupWin = window.open('', '_blank', 'width=1000,height=1000');
+            popupWin.document.open();
+            popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" /><link rel="stylesheet" type="text/css" href="styles/style.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
+            popupWin.document.close();
+        };
+
         //PDF
 
         $scope.pdf = function() {
-            html2canvas(document.getElementById("printable"), { 
+            html2canvas(document.getElementById("printable"), {
                 onrendered: function(canvas) {
                     var data = canvas.toDataURL();
                     var docDefinition = {
--- Billing.Web/scripts/controllers/ReportCtrl/SalesByCategoryCtrl.js
+++ Billing.Web/scripts/controllers/ReportCtrl/SalesByCategoryCtrl.js
@@ -1,6 +1,21 @@
 (function() {
     app.controller("SalesByCategoryCtrl", ['$scope', '$anchorScroll', 'DataService', function($scope, $anchorScroll, DataService) {
         $scope.showSalesByCategory = false;
+        $scope.requestData = {
+            startDate: new Date(2016, 1, 1),
+            endDate: new Date(2017, 1, 1)
+        };
+        $scope.openStart = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.startOpened = true;
+        };
+
+        $scope.openEnd = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.endOpened = true;
+        };
         $scope.save = function() {
             console.log("listing" + $scope.requestModel);
             DataService.insert("SalesByCategory", $scope.requestData, function(data) {
